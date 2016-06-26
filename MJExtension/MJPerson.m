370dc42f33c7b911f9a48492bf52f151b3bb9072
lmj <lmj@qq.com>
2016-06-27 01:46:34 +0800

    添加了person类

---
+++ MJExtension/MJPerson.m
@@ -0,0 +1,13 @@
+//
+//  MJPerson.m
+//  MJExtensionExample
+//
+//  Created by 林孟杰 on 16/6/27.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "MJPerson.h"
+
+@implementation MJPerson
+
+@end
